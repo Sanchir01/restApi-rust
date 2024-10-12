226a25d27c754311adc3cbe3b9e29fad073a6644
Sanchir01 <emgushovs@mail.ru>
2024-10-12 21:01:39 +0300

    actix web start rust-api

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::fmt::format;
+use actix_web::{App, HttpServer, Responder};
+
+#[actix_web::get("/greet")]
+async fn greet() -> impl Responder {
+    format!("Hello World!")
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let port = 8080;
+    println!("starting server on port {port}", );
+
+    HttpServer::new(|| App::new().service(greet))
+        .bind(("127.0.0.1", port))?
+        .workers(2)
+        .run()
+        .await
+
+
+}
